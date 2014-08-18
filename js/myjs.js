2268a66764c046eb8c4222d73f861ef87f88064b
Jay Krishna <nandyala.deepu@gmail.com>
2014-08-18 12:40:29 -0400

    added comments

--- js/myjs.js
+++ js/myjs.js
@@ -1,16 +1,21 @@
 $(document).ready(function(){
+
+	//sidbar tabs, adding active class to sidebar tabs
 	$('.sidebar').find('li').click(function(){
 		$('.active').removeClass('active');
 		$(this).addClass('active');
 	})
+	// hiding products section and  showing cart section
 	$('#cart').click(function(){
 		$('#productcontainer').css('display','none');
 		$('#cartcontainer').css('display','block');
 	})
+	// hiding cart section and  showing products section
 	$('#products').click(function(){
 		$('#productcontainer').css('display','block');
 		$('#cartcontainer').css('display','none');
 	})
+	// moving item to cart when add to cart button is clicked and add to cart button is hidden
 	$('.tocart').each(function(){
 		$(this).click(function(){
 			$(this).css('display','none');
@@ -22,7 +27,7 @@ $(document).ready(function(){
 			}, 2000);
 		})
 	})
-
+	// moving item to products from cart when add to delete button is clicked and delete button is hidden
 	$('.deletefromcart').each(function(){
 		$(this).click(function(){
 			$(this).css('display','none');
